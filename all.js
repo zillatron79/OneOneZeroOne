7f9405fd6de73a21226ec4490a91db95d2165d35
zillatron79 <shulga_oleg@ukr.net>
2026-02-16 13:52:27 +0200

    Update all.js

--- all.js
+++ all.js
@@ -11,16 +11,16 @@
           "http://wtch.ch/m", //Онлайн без преміум
           "https://lampame.github.io/main/bo.js", // Бандера Онлайн
           
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
+          "http://z01.online/live",
+          "http://smotret24.com/online.js",
+          "https://adultjs.onrender.com",
+          "https://ko31k.github.io/LMP/plugins/interface+.js",
+          "https://ko31k.github.io/LMP/plugins/UA-Finder+Mod.js",
+          "https://ko31k.github.io/LMP/plugins/rtg.js",
+          "http://lampaua.mooo.com/online/js/z0a4ctmt", / / BazarNetUA Online
+          "https://ipavlin98.github.io/lmp-plugins/series-progress-fix.js",
+          "https://bennington111.github.io/plug/collections.js",
+          "https://zillatron79.github.io/OneOneZeroOne/bat-parser.js", / / Parser
 
           "https://вашепосилання",
 
